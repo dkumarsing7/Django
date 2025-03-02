8ce80c2812dc41af16eec2721346e86fd41e7295
Deepak singh <67883350+dkumarsing7@users.noreply.github.com>
2025-03-02 09:26:09 +0530

    static files added

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -1,27 +1,13 @@
-
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+v*!(vw!2tw%t-axqd#0p_$k$m71o6ri&j00(*le6(je^vo028'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["btech-ice-cream-wala.vercel.app" ,"*" 
-                #  "btech-ice-cream-wala-lk7cia678-deepaks-projects-45030d1f.vercel.app",
-                #  "btech-ice-cream-wala-ni4gqd3lr-deepaks-projects-45030d1f.vercel.app"
-                ]
-
-
-
-# Application definition
+ALLOWED_HOSTS = ["btech-ice-cream-wala.vercel.app" ,"*" ]
 
 INSTALLED_APPS = [
     'Home.apps.HomeConfig',
@@ -63,20 +49,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Hello.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -92,11 +70,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -105,20 +78,30 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Add this so collectstatic works
 
-# added mannually
+# Static files settings
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Add this so collectstatic works
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# Database (if you plan to keep SQLite temporarily)
+if os.getenv('VERCEL'):
+    # Use an in-memory database for read-only environment
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
